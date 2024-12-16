338d1879a16ad06e2e87c4e9ce5d70a655ff187d
Jan Klausa <klausa@mercari.com>
2024-12-16 18:11:54 +0900

    clean up day16

--- Sources/Days/Day16.swift
+++ Sources/Days/Day16.swift
@@ -65,8 +65,6 @@ struct Day16: AdventDay {
     case destination
     case startPoint
 
-//    case currentPosition(CurrentPosition)
-
     init?(character: Character) {
       switch character {
       case ".": self =  .path
@@ -77,11 +75,6 @@ struct Day16: AdventDay {
       }
     }
 
-//    var stringRepresentation: String {
-//      switch self {
-//
-//      }
-//    }
   }
 
   var parsedMap: [[MapTile]] {
@@ -99,9 +92,7 @@ struct Day16: AdventDay {
     }
   }
 
-  func part1() -> Any {
-    let map = parsedMap
-
+  func findPaths(in map: [[MapTile]], priceTolerance: Int = 0) -> [Path] {
     var completePaths: [Path] = []
 
     let startingPosition = Position(row: map.indices.last! - 1,
@@ -128,7 +119,7 @@ struct Day16: AdventDay {
       }
 
       let currentPrice = prices[path.currentPosition.position, default: Int.max]
-      guard path.price < currentPrice else {
+      guard (path.price - priceTolerance) < currentPrice else {
         continue
       }
 
@@ -170,7 +161,15 @@ struct Day16: AdventDay {
       }
     }
 
-    let scores = completePaths
+    return completePaths
+  }
+
+
+
+  func part1() -> Any {
+    let map = parsedMap
+
+    let scores = findPaths(in: map)
       .map {
         $0.price
       }
@@ -182,75 +181,7 @@ struct Day16: AdventDay {
   func part2() -> Any {
     let map = parsedMap
     
-    var completePaths: [Path] = []
-    
-    let startingPosition = Position(row: map.indices.last! - 1,
-                                    column: 1)
-    
-    var pathsToCheck: Deque<Path> = [
-      .init(
-        turns: 0,
-        tilesWalked: [],
-        currentPosition:
-            .init(
-              position: startingPosition,
-              orientation: .right
-            )
-      )
-    ]
-    
-    var prices: [Position: Int] = [:]
-    
-    while let path = pathsToCheck.popFirst() {
-      guard map[path.currentPosition.position] != .destination else {
-        completePaths.append(path)
-        continue
-      }
-      
-      let currentPrice = prices[path.currentPosition.position, default: Int.max]
-      // We're deducting thousand to allow for multiple paths temporarily diverging and getting more expsensive,
-      // until they converge to reach the destination.
-      guard (path.price - 1000) <= currentPrice else {
-        continue
-      }
-      
-      prices[path.currentPosition.position] = path.price
-      
-      
-      // We don't wanna double-over on the same tile
-      guard !path.tilesWalked.contains(path.currentPosition.position) else {
-        continue
-      }
-      
-      let nextPosition = path.currentPosition.nextPosition
-      // If we can walk forward, let's just try that.
-      if !nextPosition.isWall(in: map) {
-        var newPath = path
-        newPath.tilesWalked.insert(path.currentPosition.position)
-        newPath.currentPosition = .init(position: nextPosition,
-                                        orientation: path.currentPosition.orientation)
-        
-        pathsToCheck.append(newPath)
-      }
-      
-      let rotatedPositions = path
-        .currentPosition
-        .orientation
-        .validNextDirections
-      
-      for newDirection in rotatedPositions {
-        let rotatedPosition = path.currentPosition.position.advancedBy(newDirection)
-        if !rotatedPosition.isWall(in: map) {
-          var newPath = path
-          newPath.turns += 1
-          newPath.tilesWalked.insert(path.currentPosition.position)
-          newPath.currentPosition = .init(position: rotatedPosition,
-                                          orientation: newDirection)
-          
-          pathsToCheck.append(newPath)
-        }
-      }
-    }
+    let completePaths = findPaths(in: map, priceTolerance: 1001)
 
     let lowestScore = completePaths.map { $0.price }.sorted().first!
 
