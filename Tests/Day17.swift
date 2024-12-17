e3c8805f694975a50cc3dd6c3bf2d9322972dc91
Jan Klausa <klausa@mercari.com>
2024-12-18 02:20:58 +0900

    delete useless and wrong test

--- Tests/Day17.swift
+++ Tests/Day17.swift
@@ -11,20 +11,8 @@ struct Day17Tests {
     Program: 0,1,5,4,3,0
     """
 
-  let testDataTwo = """
-  Register A: 2024
-Register B: 0
-Register C: 0
-
-Program: 0,3,5,4,3,0
-"""
   @Test func testPart1() async throws {
     let challenge = Day17(data: testData)
     #expect(String(describing: challenge.part1()) == "4,6,3,5,6,3,5,2,1,0")
-  }
-
-  @Test func testPart2() async throws {
-    let challenge = Day17(data: testDataTwo)
-    #expect(String(describing: challenge.part2()) == "117440")
   }
 }
